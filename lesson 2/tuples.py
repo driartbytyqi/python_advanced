33fa6d4abfb9db5921bb7adeb0eb1aab0c57bda5
driartbytyqi <driartbytyqii@gmail.com>
2026-04-14 20:46:22 +0200

    lesson 2

---
+++ lesson 2/tuples.py
@@ -0,0 +1,35 @@
+# # print("hello")
+#
+# names=("spam","hello","nili")
+# print(names[2])
+#
+# empty_tp=()
+#
+# print(empty_tp)
+
+# person=("nili",15,"chef")
+#
+# name,age ,profesioni=person
+#
+# print(name)
+# print(age)
+# print(profesioni)
+numrat = {"KS":"+383","AL":"+355"}
+
+Kosova = numrat["KS"]
+
+print(numrat["AL"])
+
+numrat["KS"]="+299"
+
+print(numrat["KS"])
+
+print(numrat)
+
+keys = numrat.keys()
+vlerat = numrat.values()
+print(keys)
+print(vlerat)
+
+
+
---
+++ lesson 2/tuples_dic.py
@@ -0,0 +1,15 @@
+grades = {
+    ("jon","math"):4,
+    ("robert","haxhiu"):5
+
+}
+
+print(grades[("robert","haxhiu")])
+
+grades[("jon","math")]=4
+
+print(grades[("jon","math")])
+
+suksesi = list( grades.values())
+
+print(suksesi)
